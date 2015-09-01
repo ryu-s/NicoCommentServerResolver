2a7d56710fdaf1b79a15beb7bed0082047427237
Ryu Shinmura <rysestock@gmail.com>
2015-09-01 19:52:53 +0900

    他のところで作ったコードを移行

--- NicoCommentServerResolver/Class1.cs
+++ NicoCommentServerResolver/Class1.cs
@@ -4,9 +4,381 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NicoCommentServerResolver
+namespace ryu_s
 {
-    public class Class1
+    /// <summary>
+    /// 名前はめちゃくちゃ適当。
+    /// </summary>
+    public class ProviderAddrPortResolver
     {
+        public Elem<AddrPort> AddrPortCircle { get; set; }
+        public ProviderAddrPortResolver()
+        {
+            AddrPortCircle = CreateEmptyCircle();
+        }
+        public static Elem<AddrPort> CreateEmptyCircle()
+        {
+            var element = new Elem<AddrPort>();
+            element.Before = element;
+            element.After = element;
+            return element;
+        }
+        /// <summary>
+        /// 先頭の要素を取得
+        /// </summary>
+        /// <returns></returns>
+        public Elem<AddrPort> GetHead(Elem<AddrPort> a)
+        {
+            var start = a;
+            var current = a;
+            var head = current;
+            do
+            {
+                if (IsLower(current, head))
+                    head = current;
+                current = current.After;
+            } while (current != start);
+            return head;
+        }
+        /// <summary>
+        /// aはlowerよりもlowerか
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="lower"></param>
+        /// <returns></returns>
+        public bool IsLower(Elem<AddrPort> a, Elem<AddrPort> lower)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// elemの次に要素を追加
+        /// </summary>
+        /// <param name="elem"></param>
+        /// <param name="newElem"></param>
+        public static Elem<AddrPort> InsertNext(Elem<AddrPort> elem, Elem<AddrPort> newElem)
+        {
+            newElem.After = elem.After;
+            newElem.Before = elem;
+            elem.After.Before = newElem;
+            elem.After = newElem;
+            return newElem;
+        }
+        public static Elem<AddrPort> InsertNext(Elem<AddrPort> elem, AddrPort newAddrPort)
+        {
+            return InsertNext(elem, new Elem<AddrPort>(newAddrPort));
+        }
+        public static Elem<AddrPort> InsertNext(Elem<AddrPort> elem, IEnumerable<AddrPort> newAddrPorts)
+        {
+            var tmp = elem;
+            foreach (var newAddrPort in newAddrPorts)
+            {
+                tmp = InsertNext(tmp, newAddrPort);
+            }
+            return tmp;
+        }
+
+        /// <summary>
+        /// 空要素が無くなったか
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsCompleted(Elem<AddrPort> a)
+        {
+            var start = a;
+            var current = a;
+            do
+            {
+                if (current.IsEmpty)
+                    return false;
+                current = current.After;
+            } while (current != start);
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="circle"></param>
+        /// <param name="addrPort"></param>
+        /// <returns></returns>
+        public static Elem<AddrPort> GetElement(Elem<AddrPort> circle, AddrPort addrPort)
+        {
+            var start = circle;
+            var current = circle;
+            do
+            {
+                if (!current.IsEmpty && current.Value.Equals(addrPort))
+                    return current;
+                current = current.After;
+            } while (current != start);
+            return null;
+        }
+        /// <summary>
+        /// headの後ろ、tailの前を新しい要素に付け替える。
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="tail"></param>
+        /// <param name="newAddrPorts"></param>
+        /// <returns></returns>
+        public static Elem<AddrPort> Replace(Elem<AddrPort> head, Elem<AddrPort> tail, IEnumerable<AddrPort> newAddrPorts)
+        {
+            var tmpCircle = CreateEmptyCircle();
+            InsertNext(tmpCircle, newAddrPorts);
+            var begin = tmpCircle.After;
+            var end = tmpCircle.Before;
+            head.After = begin;
+            begin.Before = head;
+            tail.Before = end;
+            end.After = tail;
+            return head;
+        }
+        public class CircleAndNotResolved<T>
+        {
+            public T NotResolved { get; private set; }
+            public Elem<AddrPort> Circle { get; private set; }
+            public CircleAndNotResolved(Elem<AddrPort> circle, T notResolved)
+            {
+                Circle = circle;
+                NotResolved = notResolved;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="circle"></param>
+        /// <param name="parts"></param>
+        /// <returns></returns>
+        public static CircleAndNotResolved<IEnumerable<IEnumerable<AddrPort>>> Concat(Elem<AddrPort> circle, IEnumerable<IEnumerable<AddrPort>> parts)
+        {
+            var ret0 = circle;
+            var list = parts;
+            int before;
+            int after;
+            var tmp = new List<IEnumerable<AddrPort>>();
+            //listの要素数が変わらなくなるまでひたすら繰り返す。
+            do
+            {
+                before = list.Count();
+                foreach (var arr in list)
+                {
+                    var ret = ProviderAddrPortResolver.Concat(ret0, arr);
+                    if (ret.NotResolved.Count() != 0)
+                        tmp.Add(ret.NotResolved);
+                    ret0 = ret.Circle;
+                }
+                list = tmp;
+                after = list.Count();
+            } while (before != after);
+            return new CircleAndNotResolved<IEnumerable<IEnumerable<AddrPort>>>(ret0, list);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="part1"></param>
+        /// <param name="part2"></param>
+        /// <returns>できたやつと結合できなかったやーつ</returns>
+        public static CircleAndNotResolved<IEnumerable<AddrPort>> Concat(Elem<AddrPort> circle, IEnumerable<AddrPort> part2)
+        {
+            //空
+            if (circle.Count == 1 && circle.IsEmpty)
+            {
+                foreach (var addrPort in part2)
+                {
+                    circle = InsertNext(circle, addrPort);
+                }
+                return new CircleAndNotResolved<IEnumerable<AddrPort>>(circle, new List<AddrPort>());
+            }
+
+            var part = part2.ToList();
+            for (int i = 0; i < part.Count; i++)
+            {
+                var start = circle;
+                var current = circle;
+                do
+                {
+                    if (!current.IsEmpty && current.Value.Equals(part[i]))
+                    {
+                        if (i == 0)
+                        {
+                            //もし、次の空で無い要素が2つ目以降と一致したら、一致したところまでをつなぎ替える
+                            var t = current;//次の空でない要素
+                            do
+                            {
+                                t = t.After;
+                            } while (t.IsEmpty);
+                            for (int m = 1; m < part.Count; m++)
+                            {
+                                if (t.Value.Equals(part[m]))
+                                {
+                                    Replace(current, t.After, part.GetRange(i + 1, m));
+                                    //                                    System.Diagnostics.Debug.WriteLine(circle.ToStr());
+                                    //一致したところ以降は最初から
+                                    return Concat(circle, part.GetRange(m, part.Count - m));
+                                }
+                            }
+
+                            //次の要素も一致したら最初の要素を削除してもう一回最初から。
+                            if (i + 1 < part.Count && !current.After.IsEmpty && current.After.Value.Equals(part[i + 1]))
+                            {
+                                part.RemoveAt(i);
+                                return Concat(circle, part);
+                            }
+
+                            //3要素目以降でもし一致したら一致したところまでを新しい要素に付け替える。これで途中にEmptyがあっても消える
+                            Elem<AddrPort> matchElem = null;
+                            int j;//一致したインデックス
+                            for (j = 2; j < part.Count; j++)
+                            {
+                                matchElem = GetElement(circle, part[j]);
+                            }
+                            if (matchElem == null)
+                            {
+                                //既に登録済みの中に今回の分の2つ目以降の要素は含まれていなかった。今回の分が1つしか無かった場合もここに来る。余分な計算をする羽目になるが、一般的に考える。
+                                if (i + 1 < part.Count)
+                                {
+                                    //2つ目以降の要素を挿入する。
+                                    InsertNext(current, part.GetRange(i + 1, part.Count - (i + 1)));
+                                }
+                                return new CircleAndNotResolved<IEnumerable<AddrPort>>(circle, new List<AddrPort>());
+                            }
+                            Replace(current, matchElem.After, part.GetRange(i + 1, part.Count - (i + j)));
+                            //var newElemList = new List<Elem<AddrPort>>();
+                            //for (int k = 1; k <= j; k++)
+                            //{
+                            //    newElemList.Add(new Elem<AddrPort>(part[k]));
+                            //}
+                            //var tmp = current;
+                            //foreach(var newElem in newElemList)
+                            //{
+                            //    tmp = InsertNext(tmp, part.GetRange(1, part.Count - (i + j)));
+                            //}
+                            if (j + 1 < part.Count)
+                            {
+                                part.RemoveRange(0, j - 1);
+                                return Concat(circle, part);
+                            }
+                        }
+                        else
+                        {
+                            //2つ目以降が一致した。
+                            //一致したところまでの要素を単純につなぎ、再度やり直す。
+                            InsertNext(current.Before, part.GetRange(0, i));
+                            part.RemoveRange(0, i);
+                            return Concat(circle, part);
+                        }
+                    }
+                    current = current.After;
+                } while (current != start);
+            }
+            //一切一致しなかった。
+            return new CircleAndNotResolved<IEnumerable<AddrPort>>(circle, part2);
+        }
+    }
+
+    public class AddrPort
+    {
+        public string Addr { get; private set; }
+        public int Port { get; private set; }
+        public AddrPort(string addr, int port)
+        {
+            Addr = addr;
+            Port = port;
+        }
+        public override string ToString()
+        {
+            return $"{Addr}, {Port}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            AddrPort p = obj as AddrPort;
+            if ((object)p == null)
+                return false;
+            return this.Equals(p);
+        }
+        public bool Equals(AddrPort a)
+        {
+            return this.Addr == a.Addr && this.Port == a.Port;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    public class Elem<T>
+    {
+        /// <summary>
+        /// 前の要素
+        /// </summary>
+        public Elem<T> Before;
+        public T Value { get; set; }
+        public bool IsEmpty
+        {
+            get
+            {
+                return Value == null;
+            }
+        }
+        /// <summary>
+        /// 次の要素
+        /// </summary>
+        public Elem<T> After;
+        public Elem()
+        {
+
+        }
+        public Elem(T v)
+        {
+            Value = v;
+        }
+        public override string ToString()
+        {
+            if (IsEmpty)
+                return $"(Empty)";
+            else
+                return Value.ToString();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public bool Equals(AddrPort a)
+        {
+            if (a == null || this.Value == null)
+                return false;
+            return this.Value.Equals(a);
+        }
+        public IEnumerable<Elem<T>> Elems()
+        {
+            var start = this;
+            var current = this;
+            do
+            {
+                yield return current;
+                current = current.After;
+            } while (current != start);
+        }
+        public int Count
+        {
+            get
+            {
+                return Elems().ToList().Count;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public string ToStr()
+        {
+            var start = this;
+            var current = this;
+            var str = "";
+            do
+            {
+                str += current.ToString() + Environment.NewLine;
+                current = current.After;
+            } while (current != start);
+            return str;
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using ryu_s;
+using System.Collections.Generic;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var resolver = new ProviderAddrPortResolver();
+            var t = new AddrPort("omsg101", 2805);
+            var k = new AddrPort("omsg102", 2818);
+            var g = new AddrPort("omsg103", 2831);
+            var ret = ProviderAddrPortResolver.Concat(ProviderAddrPortResolver.CreateEmptyCircle(), new[] { t, k, g });
+            var a = new Elem<AddrPort>(t);
+            var b = new Elem<AddrPort>(k);
+            var c = new Elem<AddrPort>(g);
+            var circle = ProviderAddrPortResolver.CreateEmptyCircle();
+            circle = ProviderAddrPortResolver.InsertNext(circle, a);
+            circle = ProviderAddrPortResolver.InsertNext(circle, b);
+            circle = ProviderAddrPortResolver.InsertNext(circle, c);
+            Assert.AreEqual(ret.Circle.ToStr(), circle.ToStr());
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var a = new[]
+            {
+                new AddrPort("omsg101" ,2815),
+                new AddrPort("omsg102" ,2828),
+                new AddrPort("omsg103" ,2841),
+                new AddrPort("omsg104" ,2854),
+                new AddrPort("omsg101" ,2816),
+                new AddrPort("omsg102" ,2829),
+                new AddrPort("omsg103" ,2842),
+                new AddrPort("omsg104" ,2855),
+                new AddrPort("omsg101" ,2817),
+                new AddrPort("omsg102" ,2830),
+                new AddrPort("omsg103" ,2843),
+                new AddrPort("omsg104" ,2856),
+                new AddrPort("omsg105" ,2867),
+                new AddrPort("omsg106" ,2880),
+                new AddrPort("omsg105" ,2868),
+                new AddrPort("omsg106" ,2881),
+                new AddrPort("omsg105" ,2869),
+                new AddrPort("omsg106" ,2882),
+            };
+            var b = new[]
+            {
+                new AddrPort("omsg106" ,2882),
+                new AddrPort("omsg101" ,2815),
+            };
+            var ret1 = ProviderAddrPortResolver.Concat(ProviderAddrPortResolver.CreateEmptyCircle(), a);
+            var ret2 = ProviderAddrPortResolver.Concat(ret1.Circle, b);
+            Assert.IsTrue(ProviderAddrPortResolver.IsCompleted(ret2.Circle));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var a = new[]
+            {
+                new AddrPort("omsg103" ,2843),
+                new AddrPort("omsg104" ,2856),
+                new AddrPort("omsg105" ,2867),
+                new AddrPort("omsg106" ,2880),
+                new AddrPort("omsg105" ,2868),
+                new AddrPort("omsg106" ,2881),
+                new AddrPort("omsg105" ,2869),
+                new AddrPort("omsg106" ,2882),
+            };
+            var b = new[]
+            {
+                new AddrPort("omsg106" ,2882),
+                new AddrPort("omsg101" ,2815),
+            };
+            var c = new[]
+            {
+                new AddrPort("omsg101" ,2815),
+                new AddrPort("omsg102" ,2828),
+                new AddrPort("omsg103" ,2841),
+                new AddrPort("omsg104" ,2854),
+            };
+
+            var d = new[]
+            {
+                new AddrPort("omsg104" ,2854),
+                new AddrPort("omsg101" ,2816),
+                new AddrPort("omsg102" ,2829),
+                new AddrPort("omsg103" ,2842),
+                new AddrPort("omsg104" ,2855),
+                new AddrPort("omsg101" ,2817),
+                new AddrPort("omsg102" ,2830),
+                new AddrPort("omsg103" ,2843),
+            };
+            var ret0 = ProviderAddrPortResolver.CreateEmptyCircle();
+            var ret1 = ProviderAddrPortResolver.Concat(ret0, a);
+            var ret2 = ProviderAddrPortResolver.Concat(ret1.Circle, b);
+            var ret3 = ProviderAddrPortResolver.Concat(ret2.Circle, c);
+            var ret4 = ProviderAddrPortResolver.Concat(ret3.Circle, d);
+            Assert.IsTrue(ProviderAddrPortResolver.IsCompleted(ret4.Circle));
+            Assert.IsTrue(ret1.NotResolved.Count() == 0);
+            Assert.IsTrue(ret2.NotResolved.Count() == 0);
+            Assert.IsTrue(ret3.NotResolved.Count() == 0);
+            Assert.IsTrue(ret4.NotResolved.Count() == 0);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var a = new[]
+            {
+                new AddrPort("omsg103" ,2843),
+                new AddrPort("omsg104" ,2856),
+                new AddrPort("omsg105" ,2867),
+                new AddrPort("omsg106" ,2880),
+                new AddrPort("omsg105" ,2868),
+                new AddrPort("omsg106" ,2881),
+                new AddrPort("omsg105" ,2869),
+                new AddrPort("omsg106" ,2882),
+            };
+            var b = new[]
+            {
+                new AddrPort("omsg101" ,2815),
+                new AddrPort("omsg102" ,2828),
+                new AddrPort("omsg103" ,2841),
+                new AddrPort("omsg104" ,2854),
+            };
+            var c = new[]
+            {
+                new AddrPort("omsg106" ,2882),
+                new AddrPort("omsg101" ,2815),
+            };            
+            var d = new[]
+            {
+                new AddrPort("omsg104" ,2854),
+                new AddrPort("omsg101" ,2816),
+                new AddrPort("omsg102" ,2829),
+                new AddrPort("omsg103" ,2842),
+                new AddrPort("omsg104" ,2855),
+                new AddrPort("omsg101" ,2817),
+                new AddrPort("omsg102" ,2830),
+                new AddrPort("omsg103" ,2843),
+            };
+            var ret0 = ProviderAddrPortResolver.CreateEmptyCircle();
+            var list = new List<IEnumerable<AddrPort>>() { a, b, c, d };
+            int before;
+            int after;
+            var tmp = new List<IEnumerable<AddrPort>>();
+            do
+            {
+                before = list.Count;
+                Console.WriteLine($"before={before}");
+                foreach (var arr in list)
+                {
+                    var ret = ProviderAddrPortResolver.Concat(ret0, arr);
+                    if (ret.NotResolved.Count() != 0)
+                        tmp.Add(ret.NotResolved);
+                    ret0 = ret.Circle;
+                }
+                list = tmp;
+                after = list.Count;
+                Console.WriteLine($"after={after}");
+            } while (before != after);
+            Console.WriteLine(ret0.ToStr());
+            Assert.IsTrue(ProviderAddrPortResolver.IsCompleted(ret0));
+        }
+    }
+}
